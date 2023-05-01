d8e15e3c239d9af82b23e78eb8f616ad823efc9a
HajarAbdullah9 <132262666+HajarAbdullah9@users.noreply.github.com>
2023-05-01 23:38:54 +0800

    Update weather.R

--- weather.R
+++ weather.R
@@ -5,26 +5,38 @@ get_weather_forcasting<- function(city_name){
     forecast_quer <- list(q = city_name, appid = '0999031957056343b00afa47c6adec5a', units= 'metric')
     response<- GET(forecast_url,query= forecast_quer)
     jsonresult<- content(response, as='parsed')
-    results<-jsonresult$list
+    results<-jsonresult
   
     for(result in results){
       city <- c(city, city_name)
-      weather <- c(weather, weather[[1]]$main)
-      visibility <- c(visibility,visibility)
-      temp <- c(temp,temp)
-      temp_min <- c(temp_min,temp_min)
-      temp_max <- c(temp_max,temp_max)
-      pressure <- c(pressure,pressure)
-      humidity <- c(humidity,humidity)
-      wind_speed <- c(wind_speed,wind_speed)
-      wind_deg <- c(wind_deg,wind_deg)
-      forecast_datetime <- c(forecast_datetime,forecast_datetime)
+      weather <- c(weather, results$weather[[1]]$main)
+      visibility <- c(visibility,results$visibility)
+      temp <- c(temp,results$main$temp)
+      temp_min <- c(temp_min,results$main$temp_min)
+      temp_max <- c(temp_max,results$main$temp_max)
+      pressure <- c(pressure,results$main$pressure)
+      humidity <- c(humidity,results$main$humidity)
+      wind_speed <- c(wind_speed,results$main$wind_speed)
+      wind_deg <- c(wind_deg,results$main$wind_deg)
+      forecast_datetime <- c(forecast_datetime,results$forecast_datetime)
       # Season column
       # Note that for season, you can hard code a season value from levels Spring, Summer, Autumn, and Winter based on your current month.
-      season <- c(season,season)
+      season <- c(season,results$main$season)
      
     }
-  
+                        df <- data.frame(
+                                     city=city,
+                                     weather=weather, 
+                                     visibility=visibility, 
+                                     temp=temp, 
+                                     temp_min=temp_min, 
+                                     temp_max=temp_max, 
+                                     pressure=pressure, 
+                                     humidity=humidity, 
+                                     wind_speed=wind_speed, 
+                                     wind_deg=wind_deg,
+                                     forecast_datetime= forecast_datetime,
+                                     season= season)
   }
   
   return(df)
