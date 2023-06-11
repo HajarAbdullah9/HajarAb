ff2de2a6285299141ee65294e1243965fdf14887
HajarAbdullah9 <132262666+HajarAbdullah9@users.noreply.github.com>
2023-06-11 21:55:47 +0800

    Update ui.r

--- ui.r
+++ ui.r
@@ -9,11 +9,25 @@ shinyUI(
   # Create a side-bar layout
   sidebarLayout(
     # Create a main panel to show cities on a leaflet map
-    mainPanel(
-      # leaflet output with id = 'city_bike_map', height = 1000
+     mainPanel(
+         leafletOutput ('city_bike_map', height = 1000)
     ),
     # Create a side bar to show detailed plots for a city
     sidebarPanel(
-      # select drop down list to select city
+        selectInput( inputId = 'City_dropdown',
+                     labe='Cities',
+                     choices = c('all',
+                                 'Seoul',
+                                 'New York',
+                                 "Paris",
+                                 'London',
+                                 "Suzhou"
+                                 )
+      ),
+      
+        plotOutput('temp_line', width= 575, height  = 250),
+        plotOutput('bike_line', click = 'plot_click',  width= 575, height  = 250),
+        verbatimTextOutput('bike_date_output'),
+        plotOutput('humidity_pred_chart', width= 575, height  = 250)
     ))
-))
+    ))
